f098f98b5b2039601041270d719435be1dcb30f8
zxl20070701 <1904314465@qq.com>
2026-05-03 20:37:19 +0800

    优化配置文件

--- config.ts
+++ config.ts
@@ -1,15 +1,25 @@
-// 环境一：Ollama
-// ollama serve
-let url = "http://localhost:11434/v1"
-let apiKey = ""
-let model = "qwen3.5"
+let { url, apiKey, model } = {
 
-// 环境二：llama.cpp
-// ./build/bin/llama-server -m ../Qwen3.5-9B-Q5_K_M.gguf --host 0.0.0.0 --port 8080
-// 模型地址： https://hf-mirror.com/unsloth/Qwen3.5-9B-GGUF
-// let url = "http://localhost:8080/v1"
-// let apiKey = ""
-// let model = ""
+    // 环境一：Ollama
+    // ollama serve
+    "ollama": {
+        url: "http://localhost:11434/v1",
+        apiKey: "",
+        model: "qwen3.5"
+    },
+
+    // 环境二：llama.cpp
+    // ./build/bin/llama-server -m ../Qwen3.5-9B-Q5_K_M.gguf --host 0.0.0.0 --port 8080
+    // 模型地址： https://hf-mirror.com/unsloth/Qwen3.5-9B-GGUF
+    "llama.cpp": {
+        url: "http://localhost:8080/v1",
+        apiKey: "",
+        model: ""
+    }
+
+}
+["ollama"]
+// ["llama.cpp"]
 
 let execArray: any = /https*:\/\/([^\/]+)(.+)?/.exec(url)
 let hostport = execArray[1].split(":")
